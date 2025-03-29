56eb40db30bf547b60578689bec6524de5ef50b1
Nemipune <nemipune@keemail.me>
2025-03-29 09:33:33 +0100

    installsh and some other small stuff

--- bin/executable_install.sh
+++ bin/executable_install.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-listbase="bemoji bitwarden bottom chezmoi firefox grim grimshoot-bin-sway github-cli hyfetch kdeconnect light mpd mpc ncmpcpp neofetch noto-fonts noto-fonts-emoji nushell sddm sddm slurp starship swaybg swayfx swaylock topgrade nerd-fonts vim waybar wofi xorg-xwayland"
-listfluff=""
+listbase="bemoji bitwarden bottom chezmoi fifm firefox flatpak grim grimshot-bin-sway github-cli hyfetch kdeconnect kid3 kitty light mpc mpd ncmpcpp neofetch nerd-fonts noto-fonts noto-fonts-emoji nushell pavucontrol sddm sddm slurp starship swaybg swayfx swayidle swaylock topgrade vim waybar wofi xorg-xwayland yt-dlp"
+listfluff="ardour audacity cameractrls gimp inkscape kdenlive lutris piper steam"
+config="$HOME/.config/"
 
+read -p "Enter username: " user
+
+# ensuring the rest of the system and the mirrors are up to date
 yay -Syyu
 
-# todo:
-# - prompt if you wanna install the fluff
-# - set user groups and perms and shit
-# - user_dir
-# - unrelated but check environment variables in the bashrc
-# - services enabling etc
-# - chezmoi
-# - check aegis config and packages
+# installing the base packages
+yay -S $listbase
+
+echo "Do you wish to install the floof? (graphic/gaming programs)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) yay -S $listfluff; break;;
+        No ) exit;;
+    esac
+done
+
+# chezmoi
+chezmoi init https://github.com/nemipune/dotfiles.git
+chezmoi diff
+chezmoi apply
+
+# sddm setup
+systemctl enable sddm.service
+cp -rv $config/system/sddm/sddm.conf.d /etc
+cp -rv $config/system/sddm/themes /usr/share/sddm
+cp -rv $config/system/wayland-sessions /usr/share # this one passes the "--unsupported-gpu" flag to the sway command
+
+# user config
+usermod -aG audio,seat,users,video $user
+
+# mpd setup
+systemctl --user enable mpd.service
