1eaa43629ce593bc765fac362d246665a21d2307
Artemis <nemipune@keemail.me>
2025-03-29 08:39:51 +0100

    install

---
+++ bin/executable_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+listbase="bemoji bitwarden bottom chezmoi firefox grim grimshoot-bin-sway github-cli hyfetch kdeconnect light mpd mpc ncmpcpp neofetch noto-fonts noto-fonts-emoji nushell sddm sddm slurp starship swaybg swayfx swaylock topgrade nerd-fonts vim waybar wofi xorg-xwayland"
+listfluff=""
+
+yay -Syyu
+
+# todo:
+# - prompt if you wanna install the fluff
+# - set user groups and perms and shit
+# - user_dir
+# - unrelated but check environment variables in the bashrc
+# - services enabling etc
+# - chezmoi
+# - check aegis config and packages
